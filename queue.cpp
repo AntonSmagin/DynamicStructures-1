ee63d7c9d9eee0c51d11a582d651e8dc2ae1182d
VaccineDevil <raving3475@mail.ru>
2015-10-01 17:34:31 +0300

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "queue.h"
 #include <iostream>
-
+#include <clocale>
 
 using namespace std;
 
@@ -34,21 +34,68 @@ int del(Node **pbeg)
 {
 	int temp = (*pbeg)->val;
 	Node *pv = *pbeg;
-	*pbeg = (*pbeg)->p;
-	delete pv;
-	return temp;
+		*pbeg = (*pbeg)->p;
+		cout << "Element was deleted\n";
+		delete pv;
+		return temp;
 }
 
 
 void print(Node **pbeg)
 {
-	int temp = NULL;
-	while (pbeg != NULL)
+	Node *tmp = (*pbeg)->p;
+	
+	printf("%d ", (*pbeg)->val);
+	while (tmp != NULL)
 	{
-		temp = (*pbeg)->val;
-		printf("%d ", temp);
-		*pbeg = (*pbeg)->p;
+			printf("%d ", tmp->val);
+			tmp = tmp->p;
+	}
+	cout << endl;
+	
+}
+
+void menu()
+{
+	setlocale(LC_ALL,"Russian");
+	cout << "***QUEUE***" << endl << endl;
+	Node *pbeg = NULL;
+	Node *pend = NULL;
+	{
+		cout << "1. Добавить первый элемент в очередь\n";
+		cout << "2. Добавить новый элемент в очередь\n";
+		cout << "3. Удалить первый элемент из очереди\n";
+		cout << "4. Распечатать элемент(ы) очереди\n\n";
+		cout << "Выберите число и нажмите ENTER\n";
+		int t;
+		int first_el;
+		int el = 0;
+		do
+		{
+			cin >> t;
+			switch (t)
+			{
+			case 1:
+
+				cin >> first_el;
+				pbeg = First(first_el);
+				pend = pbeg;
+			case 2:
+
+				cout << "Напишите новый элемент\n";
+				cin >> el;
+				add(&pend, el);
+			case 3:
+				if (pbeg == NULL)
+					cout << "В очереди нет элементов\n";
+				else
+					del(&pbeg);
+
+			case 4:
+				cout << "Распечатаная очередь\n";
+				print(&pbeg);
+			}
+		} while (t);
 		
 	}
-
 }
