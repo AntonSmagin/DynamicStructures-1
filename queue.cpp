7c5a7506b802ca28f70f12318d432056fd0a3d07
VaccineDevil <raving3475@mail.ru>
2015-10-08 14:33:41 +0300

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -5,13 +5,6 @@
 
 using namespace std;
 
-//Node *pbeg = First(10);
-//Node *pend = pbeg;
-
-// Node *pbeg = NULL, *pend = NULL;
-//
-
-
 Node *First(int val)
 {
 	Node *pv = new Node;
@@ -34,68 +27,89 @@ int del(Node **pbeg)
 {
 	int temp = (*pbeg)->val;
 	Node *pv = *pbeg;
-		*pbeg = (*pbeg)->p;
-		cout << "Element was deleted\n";
-		delete pv;
-		return temp;
+	*pbeg = (*pbeg)->p;
+	delete pv;
+	return temp;
 }
 
 
 void print(Node **pbeg)
 {
 	Node *tmp = (*pbeg)->p;
-	
+
 	printf("%d ", (*pbeg)->val);
 	while (tmp != NULL)
 	{
-			printf("%d ", tmp->val);
-			tmp = tmp->p;
+		printf("%d ", tmp->val);
+		tmp = tmp->p;
 	}
 	cout << endl;
-	
+
 }
 
 void menu()
 {
-	setlocale(LC_ALL,"Russian");
-	cout << "***QUEUE***" << endl << endl;
+	setlocale(LC_ALL, "Russian");
+
 	Node *pbeg = NULL;
 	Node *pend = NULL;
 	{
-		cout << "1. Добавить первый элемент в очередь\n";
-		cout << "2. Добавить новый элемент в очередь\n";
-		cout << "3. Удалить первый элемент из очереди\n";
-		cout << "4. Распечатать элемент(ы) очереди\n\n";
-		cout << "Выберите число и нажмите ENTER\n";
-		int t;
+
+		char t;
 		int first_el;
 		int el = 0;
 		do
 		{
+			cout << "***QUEUE***" << endl << endl;
+			cout << "1. Добавить первый элемент в очередь\n";
+			cout << "2. Добавить новый элемент в очередь\n";
+			cout << "3. Удалить первый элемент из очереди\n";
+			cout << "4. Распечатать элемент(ы) очереди\n\n";
+			cout << "Выберите число и нажмите ENTER\n";
 			cin >> t;
-			switch (t)
+			if (t >= 48 && t < 53)
 			{
-			case 1:
+				switch (t)
+				{
+				case 1:
+					cout << "Создайте первый элемент\n\n";
+					cin >> first_el;
 
-				cin >> first_el;
-				pbeg = First(first_el);
-				pend = pbeg;
-			case 2:
+					pbeg = First(first_el);
+					pend = pbeg;
+					system("CLS");
+					break;
 
-				cout << "Напишите новый элемент\n";
-				cin >> el;
-				add(&pend, el);
-			case 3:
-				if (pbeg == NULL)
-					cout << "В очереди нет элементов\n";
-				else
+				case 2:
+					cout << "Напишите новый элемент\n\n";
+					cin >> el;
+					add(&pend, el);
+					system("CLS");
+					break;
+				case 3:
 					del(&pbeg);
-
-			case 4:
-				cout << "Распечатаная очередь\n";
-				print(&pbeg);
+					cout << "Элемент был удален из очереди\n\n";
+					break;
+				case 4:
+					cout << "Распечатаная очередь\n\n";
+					print(&pbeg);
+					system("pause");
+					cout << endl;
+					system("cls");
+					break;
+				}
+			}
+			else 
+			{
+				cout << endl;
+				cout << "Неверное значение! Введиите число от 1 до 4"<<endl<<endl;
+				system("pause");
+				cout << endl;
+				system("cls");
 			}
 		} while (t);
 		
+		
 	}
+
 }
