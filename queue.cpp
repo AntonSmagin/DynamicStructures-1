10d1b069f7588c784d11d4bbdd2ef7c0e36cbba4
VaccineDevil <raving3475@mail.ru>
2015-10-10 11:35:47 +0300

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -2,6 +2,8 @@
 #include "queue.h"
 #include <iostream>
 #include <clocale>
+#include <ctype.h>
+
 
 using namespace std;
 
@@ -25,6 +27,7 @@ void add(Node **pend, int val)
 
 int del(Node **pbeg)
 {
+
 	int temp = (*pbeg)->val;
 	Node *pv = *pbeg;
 	*pbeg = (*pbeg)->p;
@@ -67,16 +70,28 @@ void menu()
 			cout << "4. Распечатать элемент(ы) очереди\n\n";
 			cout << "Выберите число и нажмите ENTER\n";
 			cin >> t;
-			if (t >= 48 && t < 53)
+			isdigit(t);
+			int c;
+			c = t - '0';
+			if (c >=0 && c < 5)
 			{
-				switch (t)
+				
+				switch (c)
 				{
 				case 1:
-					cout << "Создайте первый элемент\n\n";
-					cin >> first_el;
+					if (pbeg == NULL)
+					{
+						cout << "Создайте первый элемент\n\n";
+						cin >> first_el;
 
-					pbeg = First(first_el);
-					pend = pbeg;
+						pbeg = First(first_el);
+						pend = pbeg;
+					}
+					else 
+					{ 
+						cout << "Ошибка! Первый элемент существует!\n\n";
+					    system("pause");
+					}
 					system("CLS");
 					break;
 
@@ -87,8 +102,17 @@ void menu()
 					system("CLS");
 					break;
 				case 3:
-					del(&pbeg);
-					cout << "Элемент был удален из очереди\n\n";
+					if (pbeg != NULL){
+						del(&pbeg);
+						cout << "Элемент был удален из очереди\n\n";
+					}
+					else {
+						cout << endl;
+						cout << "Ошибка! Очередь пуста!\n\n";
+					}
+					system("pause");
+					cout << endl;
+					system("cls");
 					break;
 				case 4:
 					cout << "Распечатаная очередь\n\n";
@@ -99,17 +123,19 @@ void menu()
 					break;
 				}
 			}
-			else 
+			else
 			{
 				cout << endl;
-				cout << "Неверное значение! Введиите число от 1 до 4"<<endl<<endl;
+				cout << "Неверное значение! Введиите число от 1 до 4" << endl << endl;
 				system("pause");
 				cout << endl;
 				system("cls");
+				
+				
 			}
-		} while (t);
-		
-		
+		} while (1);
+
+
 	}
 
 }
